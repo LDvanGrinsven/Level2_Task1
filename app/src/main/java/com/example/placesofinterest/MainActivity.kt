eb3701588c630848850a90841ff6787bb9e5b1a3
Leander van Grinsven <leander550@live.nl>
2020-03-21 19:30:59 +0100

    Level2 Task1 Completed

---
+++ app/src/main/java/com/example/placesofinterest/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.example.placesofinterest
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.recyclerview.widget.StaggeredGridLayoutManager
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    private val places = arrayListOf<Place>()
+    private val placeAdapter = PlaceAdapter(places)
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val arrayList = ArrayList<Place>()
+        initViews()
+    }
+    fun initViews(){
+        rvPlaces.layoutManager = StaggeredGridLayoutManager(2, 1)
+        rvPlaces.adapter = placeAdapter
+
+        for (i in Place.PLACE_NAMES.indices) {
+            places.add(Place(Place.PLACE_NAMES[i], Place.PLACE_RES_DRAWABLE_IDS[i]))
+        }
+        placeAdapter.notifyDataSetChanged()
+    }
+}
+
+
---
+++ app/src/main/java/com/example/placesofinterest/PlaceAdapter.kt
@@ -0,0 +1,43 @@
+package com.example.placesofinterest
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.item_place.view.*
+
+class PlaceAdapter(private val reminders: List<Place>) : RecyclerView.Adapter<PlaceAdapter.ViewHolder>() {
+
+    /**
+     * Creates and returns a ViewHolder object, inflating the layout called item_reminder.
+     */
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        return ViewHolder(
+            LayoutInflater.from(parent.context).inflate(R.layout.item_place, parent, false)
+        )
+    }
+
+    /**
+     * Returns the size of the list
+     */
+    override fun getItemCount(): Int {
+        return reminders.size
+    }
+
+    /**
+     * Called by RecyclerView to display the data at the specified position.
+     */
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bind(reminders[position])
+    }
+
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+        fun bind(place: Place) {
+            itemView.ivPlace.setImageResource(place.imageResId)
+            itemView.tvPlace.text = place.name
+        }
+    }
+
+}
